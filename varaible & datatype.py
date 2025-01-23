735035c3f241d66cf84f645d8eb97b910181b779
Dhanasekar <115201732+dhanasekardevops@users.noreply.github.com>
2025-01-23 12:48:41 +0530

    Update varaible & datatype.py

--- varaible & datatype.py
+++ varaible & datatype.py
@@ -11,3 +11,10 @@ print(name,type(name))
 print(age,type(age))
 print(version,type(version))
 print(is_enabled,type(is_enabled))
+
+Result :
+
+Dhanasekar <class 'str'>
+25 <class 'int'>
+1.01 <class 'float'>
+True <class 'bool'>
