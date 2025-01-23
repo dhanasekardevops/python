8904122d608c0c8b63215e1597c7458e342838da
Dhanasekar <115201732+dhanasekardevops@users.noreply.github.com>
2025-01-23 15:36:24 +0530

    Create data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,46 @@
+# syntax of list
+#add the values and allows duplicates
+#variable_name =[x,y,z]
+
+List=[1,2,3,4]
+print(List)
+List.append(5)
+print(List)
+print(List[0])
+print(List[4])
+print(List[3])
+
+
+
+# syntax of Dictionary
+#somedic ={"key1":"value1","key2":"value2"}
+Dictionary={"staff_1_salary":"10000","staff_2_salary":"20000"}
+print(Dictionary["staff_1_salary"])
+print(Dictionary["staff_2_salary"])
+
+# syntax of Tuple
+#variable_name =(x,y,z)
+#Cannot add the values and alows duplicates
+Tuple=(1,2,3,4)
+print(Tuple)
+
+
+# syntax of Set
+#variable_name ={x,y,z}
+#add the values and didn't allows duplicates
+
+Set={1,4,5,4,5}
+Set.add(2)
+print(Set)
+
+Result:
+[1, 2, 3, 4]
+[1, 2, 3, 4, 5]
+1
+5
+4
+10000
+20000
+(1, 2, 3, 4)
+{1, 2, 4, 5}
+
