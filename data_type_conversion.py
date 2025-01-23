50cfc89938064f6c7c1e664792915c006b11b954
Dhanasekar <115201732+dhanasekardevops@users.noreply.github.com>
2025-01-23 12:45:57 +0530

    Add files via upload

---
+++ data_type_conversion.py
@@ -0,0 +1,19 @@
+#Assigning Variables
+name ="Dhanasekar"
+age= 25
+version =1.01
+is_enabled= True
+
+#convert data type to another
+
+age_str=str(age)
+version_float=int(version)
+
+# int(): Converts to integer.
+# float(): Converts to float.
+# str(): Converts to string.
+# bool(): Converts to boolean
+
+print(age_str,type(age_str))
+print(version_float,type(version_float))
+
