ba2593a4359d7dab743e29bfe724c8a962c1f046
Dhanasekar <115201732+dhanasekardevops@users.noreply.github.com>
2025-01-23 12:42:39 +0530

    Add files via upload

---
+++ varaible_datatype.py
@@ -0,0 +1,13 @@
+
+#Assigning Variables
+name ="Dhanasekar"
+age= 25
+version =1.01
+is_enabled= True
+
+#Execute the variables and thier types
+
+print(name,type(name))
+print(age,type(age))
+print(version,type(version))
+print(is_enabled,type(is_enabled))
