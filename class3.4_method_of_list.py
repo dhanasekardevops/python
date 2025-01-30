fd267a3a3bc2aa246717b3936bbbfbb20d800101
Dhanasekar <115201732+dhanasekardevops@users.noreply.github.com>
2025-01-30 18:21:57 +0530

    Add files via upload

---
+++ class3.4_method_of_list.py
@@ -0,0 +1,16 @@
+#Methods
+# list.index(item, start=0, end=len(list)): Returns the index of the first occurrence of an item.
+dhanalist2=(2345,6789,1234,5678,5678,5678,5678,2345)
+print(dhanalist2.index(5678))
+# print(dhanalist2.count(2345))
+
+# list.count(item): Returns the count of an item.
+
+print(dhanalist2.count(5678))
+print(dhanalist2.count(2345))
+
+
+
+# list.sort(reverse=False): Sorts the list in ascending order.
+# list.reverse(): Reverses the order of elements.
+# list.copy(): Returns a shallow copy of the list.
